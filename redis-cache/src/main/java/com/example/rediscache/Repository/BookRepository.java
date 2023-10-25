d14ba98f43823013cd3bbf4cc81f935aa64838a4
Naveena Krishna Gouda <55161387+naveengowda6197@users.noreply.github.com>
2023-10-25 11:48:45 +0530

    Update BookRepository.java

--- redis-cache/src/main/java/com/example/rediscache/Repository/BookRepository.java
+++ redis-cache/src/main/java/com/example/rediscache/Repository/BookRepository.java
@@ -4,8 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.rediscache.Entity.Book;
-@Repository
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Integer> {
 	
 }
